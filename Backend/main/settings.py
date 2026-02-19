60f030de375fb58aa3f8e00938dc0a9d3dbc89ad
vishal Singh <vs9771826941@gmail.com>
2026-02-19 15:51:47 +0530

    fix: added credentials in env file

--- Backend/main/settings.py
+++ Backend/main/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -174,5 +175,5 @@ EMAIL_HOST = "smtp.gmail.com"
 PASSWORD_RESET_TIMEOUT = 1800
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "vishalsinghdeveloper004@gmail.com"
-EMAIL_HOST_PASSWORD = "ekptsjbxepafulap"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
