37ea64b66685906678f40ea7390e234f984e110c
vishal Singh <vs9771826941@gmail.com>
2026-02-23 15:40:53 +0530

    chnages done for deployment

--- Backend/main/settings.py
+++ Backend/main/settings.py
@@ -23,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k&+4lr_j2aq8yhe14hh=nm9&4wvorn4mit#00huvq6_&qir--#'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "VishalSinghDeveloper.pythonanywhere.com",]
 
 # Application definition
 
